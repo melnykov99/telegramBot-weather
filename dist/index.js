0b886c5f5e651f7ce02fe33f7d4f9d34da6131c1
Melnykov Dmitry Hobby <melnykov.ds99@gmail.com>
2023-12-10 20:07:00 +0300

    add handling condition code

--- dist/index.js
+++ dist/index.js
@@ -25,9 +25,71 @@ bot.on("message", (ctx) => __awaiter(void 0, void 0, void 0, function* () {
     const city = ctx.message.text;
     try {
         const response = yield axios_1.default.get(`${host}/current.json?key=${weatherApiKey}&q=${city}&lang=ru`);
-        console.log(response.data);
-        const answer = `Погода в городе ${response.data.location.name} \nТемпература: ${response.data.current.temp_c} \nСостояние: ${response.data.current.condition.text} \nСкорость ветра: ${response.data.current.wind_mph} м/с`;
-        yield ctx.reply(answer);
+        const temperatureText = response.data.current.temp_c < 0 ? `Температура: ${response.data.current.temp_c}°C🥶` : `Температура: ${response.data.current.temp_c}°C😊`;
+        let conditionText = '123';
+        switch (response.data.current.condition.code) {
+            case 1000:
+                conditionText = `На улице ${response.data.current.condition.text} ☀️`;
+                break;
+            case 1003:
+            case 1006:
+            case 1009:
+            case 1030:
+            case 1135:
+            case 1147:
+                conditionText = `На улице ${response.data.current.condition.text} ☁️`;
+                break;
+            case 1063:
+            case 1072:
+            case 1087:
+            case 1150:
+            case 1153:
+            case 1168:
+            case 1171:
+            case 1180:
+            case 1183:
+            case 1186:
+            case 1189:
+            case 1192:
+            case 1195:
+            case 1198:
+            case 1201:
+            case 1240:
+            case 1243:
+            case 1246:
+            case 1249:
+            case 1252:
+            case 1261:
+            case 1264:
+            case 1273:
+            case 1276:
+                conditionText = `На улице ${response.data.current.condition.text} 🌧`;
+                break;
+            case 1066:
+            case 1069:
+            case 1114:
+            case 1117:
+            case 1204:
+            case 1207:
+            case 1210:
+            case 1213:
+            case 1216:
+            case 1219:
+            case 1222:
+            case 1225:
+            case 1237:
+            case 1255:
+            case 1258:
+            case 1279:
+            case 1282:
+                conditionText = `На улице ${response.data.current.condition.text} ❄️`;
+                break;
+            default:
+                conditionText = `На улице ${response.data.current.condition.text}!`;
+        }
+        const windText = `Скорость ветра: ${response.data.current.wind_mph} м/с💨`;
+        const fullAnswer = `Погода в городе <b>${response.data.location.name}</b> 🌇\n${temperatureText} \n${conditionText} \n${windText}`;
+        yield ctx.reply(fullAnswer, { parse_mode: "HTML" });
     }
     catch (error) {
         console.log(error);
