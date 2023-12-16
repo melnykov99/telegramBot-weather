bc285f51371a6a33540709db3487f17804b26598
Melnykov Dmitry Hobby <melnykov.ds99@gmail.com>
2023-12-16 15:32:40 +0300

    add forecastEverydayCron

--- api/cron.ts
+++ api/cron.ts
@@ -7,11 +7,11 @@ import {DB_RESULT} from "./constants";
 async function forecastEverydayCron() {
     const data = await usersRepository.getAllUsers()
     if (data === DB_RESULT.UNKNOWN_ERROR) {
-        return
+        return console.log('crone error. db error')
     }
     const usersCount = data.rowCount
     if(!usersCount) {
-        return
+        return console.log('crone error. data.rowCount null')
     }
     const usersData = data.rows
     const togetherDate = new Date().toISOString().split('T')[0]
@@ -20,36 +20,15 @@ async function forecastEverydayCron() {
         const answer: string = await weatherService.forecastByDate(chatId, togetherDate)
         await bot.api.sendMessage(usersData[i].chatId, answer, {parse_mode: "HTML", reply_markup: mainKeyboard})
     }
+    console.log('crone successfully completed')
 }
-
-async function testCron() {
-    const data = await usersRepository.testFound(283517479)
-    if (data === DB_RESULT.UNKNOWN_ERROR) {
-        return
-    }
-    if (data === DB_RESULT.NOT_FOUND) {
-        return
-    }
-    const usersCount = data.rowCount
-    if(!usersCount) {
-        return
-    }
-    const usersData = data.rows
-    const togetherDate = new Date().toISOString().split('T')[0]
-    for (let i = 0; i < usersCount; i++) {
-        const chatId = usersData[i].chatId
-        const answer: string = await weatherService.forecastByDate(chatId, togetherDate)
-        await bot.api.sendMessage(usersData[i].chatId, answer, {parse_mode: "HTML", reply_markup: mainKeyboard})
-    }
-}
-
 // Экспортируем функцию для обработки HTTP-запросов
 export default async (req: NowRequest, res: NowResponse) => {
     try {
-        await testCron(); // Вызываем вашу функцию
+        await forecastEverydayCron(); // Вызываем вашу функцию
         res.status(200).send('Cron job completed successfully');
     } catch (error) {
         console.error(error);
-        res.status(500).send('Internal Server Error');
+        res.status(500).send('Crone Internal Server Error');
     }
 };
