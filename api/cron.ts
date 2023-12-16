5b17101dff98c58d6a4037bb90f21fd696f9bf96
Melnykov Dmitry Hobby <melnykov.ds99@gmail.com>
2023-12-16 11:02:32 +0300

    fix cron vercel

--- api/cron.ts
+++ api/cron.ts
@@ -1,6 +1,7 @@
-import {usersRepository} from "./db";
-import {weatherService} from "./weatherService";
-import {bot} from "./bot";
+import { NowRequest, NowResponse } from '@vercel/node';
+import { usersRepository } from "./db";
+import { weatherService } from "./weatherService";
+import { bot } from "./bot";
 
 export async function testCron() {
     console.log('крона начало')
@@ -8,6 +9,17 @@ export async function testCron() {
     console.log('data')
     const chatId = 283517479;
     const togetherDate = new Date().toISOString().split('T')[0]
-    const answer = await weatherService.forecastByDate(chatId,togetherDate)
+    const answer = await weatherService.forecastByDate(chatId, togetherDate)
     await bot.api.sendMessage(chatId, answer)
 }
+
+// Экспортируем функцию для обработки HTTP-запросов
+export default async (req: NowRequest, res: NowResponse) => {
+    try {
+        await testCron(); // Вызываем вашу функцию
+        res.status(200).send('Cron job completed successfully');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+};
