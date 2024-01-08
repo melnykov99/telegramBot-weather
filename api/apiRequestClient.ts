7e72e6541a8ceb32ba3dffeaacedf0773090af23
Melnykov Dmitry Hobby <melnykov.ds99@gmail.com>
2024-01-08 19:12:51 +0300

    change api request address

--- api/apiRequestClient.ts
+++ api/apiRequestClient.ts
@@ -6,14 +6,14 @@ dotenv.config();
 
 
 const weatherApiKey: string | undefined = process.env.WEATHER_API_KEY;
-const weatherHost: string = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline";
+const weatherHost: string = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline';
 
 //Methods for requesting weather via weather.visualcrossing.com
 export const apiRequestClient = {
     //Request weather. In period may be "today", "tomorrow", "next 2 days", "next 4 days"
     async forecastRequest(city: string, period: string) {
         try {
-            return await axios.get(`${weatherHost}/${city}/${period}?key=${weatherApiKey}&lang=ru&unitGroup=uk&include=days`);
+            return await axios.get(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/${period}?key=${weatherApiKey}&lang=ru&unitGroup=uk&include=days`);
         } catch (error) {
             return API_RESULT.UNKNOWN_ERROR;
         }
@@ -21,7 +21,7 @@ export const apiRequestClient = {
     //Test request which use for validation city. If city exists return city name, else return error
     async checkCity(city: string) {
         try {
-            const response = await axios.get(`${weatherHost}/${city}/today?key=${weatherApiKey}&lang=ru&unitGroup=uk&include=days`);
+            const response = await axios.get(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/today?key=${weatherApiKey}&lang=ru&unitGroup=uk&include=days`);
             return response.data.resolvedAddress;
         } catch (error) {
             return API_RESULT.UNKNOWN_ERROR;
