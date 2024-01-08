072bf690ee72a74f83134a2a4a5f21d3942563d9
Melnykov Dmitry Hobby <melnykov.ds99@gmail.com>
2024-01-08 18:59:26 +0300

    fix bugs

--- api/apiRequestClient.ts
+++ api/apiRequestClient.ts
@@ -13,7 +13,7 @@ export const apiRequestClient = {
     //Request weather. In period may be "today", "tomorrow", "next 2 days", "next 4 days"
     async forecastRequest(city: string, period: string) {
         try {
-            return await axios.get(`${weatherHost}/${city}/${period}?key=${weatherApiKey}&lang=ru&unitGroup=uk&include=days`);
+            return await axios.get(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Rostov-on-Don/today/?key=${weatherApiKey}&lang=ru&unitGroup=uk&include=days`);
         } catch (error) {
             return API_RESULT.UNKNOWN_ERROR;
         }
@@ -21,7 +21,7 @@ export const apiRequestClient = {
     //Test request which use for validation city. If city exists return city name, else return error
     async checkCity(city: string) {
         try {
-            const response = await axios.get(`${weatherHost}/${city}/today}?key=${weatherApiKey}&lang=ru&unitGroup=uk&include=days`);
+            const response = await axios.get(`${weatherHost}/${city}/today?key=${weatherApiKey}&lang=ru&unitGroup=uk&include=days`);
             return response.data.resolvedAddress;
         } catch (error) {
             return API_RESULT.UNKNOWN_ERROR;
